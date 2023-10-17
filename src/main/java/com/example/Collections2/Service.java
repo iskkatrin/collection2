4dea9d51f6101040b36c474cce665d2028a27b3f
Ekaterina <klarina2409@gmail.com>
2023-10-17 12:28:47 +0300

    Мастер

--- src/main/java/com/example/Collections2/Service.java
+++ src/main/java/com/example/Collections2/Service.java
@@ -4,6 +4,7 @@ import java.util.*;
 
 public class Service {
     public static void main(String[] args) {
+
         System.out.println("Задание 1");
 
         List<Integer> nums = new ArrayList<>(List.of(1, 1, 2, 3, 4, 4, 5, 5, 6, 7));
